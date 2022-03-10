e8c366be54c38c33d61ecb238ee4d4af178c01d2
truefibot <truebot.github@trusttoken.com>
2022-03-10 10:13:57 +0000

    💪 Add deposit and transfer strategies linked to OPM whitelist (#1073)

---
+++ strategies/OPMDepositStrategy.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import {IBasePortfolio} from "../interfaces/IBasePortfolio.sol";
+import {DSRegistryServiceInterface} from "../interfaces/OPM/DSRegistryServiceInterface.sol";
+
+contract OPMDepositStrategy {
+    DSRegistryServiceInterface public registryService;
+
+    constructor(DSRegistryServiceInterface _registryService) {
+        registryService = _registryService;
+    }
+
+    function deposit(IBasePortfolio portfolio, uint256 amount) public {
+        require(_isValidInvestor(msg.sender), "OPMDepositStrategy: Deposit wallet not associated with a valid investor");
+        portfolio.deposit(amount, msg.sender);
+    }
+
+    function _isValidInvestor(address _address) private view returns (bool) {
+        return registryService.isInvestor(registryService.getInvestor(_address));
+    }
+}
---
+++ strategies/OPMTransferStrategy.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import {ITransferStrategy} from "../interfaces/ITransferStrategy.sol";
+import {DSRegistryServiceInterface} from "../interfaces/OPM/DSRegistryServiceInterface.sol";
+
+contract OPMTransferStrategy is ITransferStrategy {
+    DSRegistryServiceInterface public registryService;
+
+    constructor(DSRegistryServiceInterface _registryService) {
+        registryService = _registryService;
+    }
+
+    function canTransfer(
+        address from,
+        address to,
+        uint256
+    ) external view returns (bool) {
+        return _isValidInvestor(from) && _isValidInvestor(to);
+    }
+
+    function _isValidInvestor(address _address) private view returns (bool) {
+        return registryService.isInvestor(registryService.getInvestor(_address));
+    }
+}
